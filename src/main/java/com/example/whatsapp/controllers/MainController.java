183f2ecec6537a7c144c09101f539b833f0cca57
Vijay Kumar <wijay.outlook.in>
2022-10-24 17:06:44 +0530

    post method modification

--- src/main/java/com/example/whatsapp/controllers/MainController.java
+++ src/main/java/com/example/whatsapp/controllers/MainController.java
@@ -14,38 +14,7 @@ import java.util.Map;
 
 @RestController
 public class MainController {
-	
-	String inputJson = "{\r\n"
-			+ "  \"object\": \"whatsapp_business_account\",\r\n"
-			+ "  \"entry\": [{\r\n"
-			+ "      \"id\": \"WHATSAPP_BUSINESS_ACCOUNT_ID\",\r\n"
-			+ "      \"changes\": [{\r\n"
-			+ "          \"value\": {\r\n"
-			+ "              \"messaging_product\": \"whatsapp\",\r\n"
-			+ "              \"metadata\": {\r\n"
-			+ "                  \"display_phone_number\": PHONE_NUMBER,\r\n"
-			+ "                  \"phone_number_id\": PHONE_NUMBER_ID\r\n"
-			+ "              },\r\n"
-			+ "              \"contacts\": [{\r\n"
-			+ "                  \"profile\": {\r\n"
-			+ "                    \"name\": \"NAME\"\r\n"
-			+ "                  },\r\n"
-			+ "                  \"wa_id\": PHONE_NUMBER\r\n"
-			+ "                }],\r\n"
-			+ "              \"messages\": [{\r\n"
-			+ "                  \"from\": PHONE_NUMBER,\r\n"
-			+ "                  \"id\": \"wamid.ID\",\r\n"
-			+ "                  \"timestamp\": TIMESTAMP,\r\n"
-			+ "                  \"text\": {\r\n"
-			+ "                    \"body\": \"MESSAGE_BODY\"\r\n"
-			+ "                  },\r\n"
-			+ "                  \"type\": \"text\"\r\n"
-			+ "                }]\r\n"
-			+ "          },\r\n"
-			+ "          \"field\": \"messages\"\r\n"
-			+ "        }]\r\n"
-			+ "  }]\r\n"
-			+ "}";
+
 
 	@Value("${TOKEN}")
 	private String token;
@@ -59,7 +28,7 @@ public class MainController {
 	}
 
 	//to verify the callback url from dashboard side - cloud api side
-	@GetMapping("/webhook")
+	@GetMapping("/webhooks")
 	public ResponseEntity<Object> verifyCallbackUrlFromDashboard(@RequestParam(name = "hub.mode") String mode,
 																 @RequestParam(name = "hub.challenge") String challenge,
 																 @RequestParam(name = "hub.verify_token") String verify_token) {
@@ -77,7 +46,7 @@ public class MainController {
 
 	}
 
-	@PostMapping("/webhook")
+	@PostMapping("/webhooks")
 	public void fetchResponseAndReply(@RequestBody Map qparams){
 
 		JSONObject inputJsonOBject = new JSONObject(qparams);
@@ -92,12 +61,10 @@ public class MainController {
 		ParseDynamicJson.getKey(inputJsonOBject, "body");
 		String msg_body = ParseDynamicJson.po;
 
-		System.out.println("phone_number_id:" + phone_number_id + ", " +
-				"from:" + from + ", " +
-				"msg_body:" + msg_body);
+		System.out.println(inputJsonOBject.toString());
 
 
-		// create request body
+//		Create request body
 		JSONObject text = new JSONObject();
 		text.put("body", "Hi.. I'm Vijay...");
 		JSONObject request = new JSONObject();
@@ -106,29 +73,22 @@ public class MainController {
 		request.put("text", text);
 
 
-// set headers
+// 		set headers
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
 
-// send request and parse result
+// 		send request and parse result
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "https://graph.facebook.com/v13.0/"+phone_number_id+"/messages?access_token="+token;
 		ResponseEntity<String> sendResponse = restTemplate
 				.exchange(url, HttpMethod.POST, entity, String.class);
 		if (sendResponse.getStatusCode() == HttpStatus.OK) {
 			JSONObject userJson = new JSONObject(sendResponse.getBody());
+			System.out.println(userJson.toString());
 		} else if (sendResponse.getStatusCode() == HttpStatus.UNAUTHORIZED) {
 			// nono... bad credentials
 		}
-
-//		ClientDetails clientDetails = new ClientDetails(v1,
-//				v2,
-//				v3,
-//				v4);
-//		return clientDetails;
-
-
 
 	}
 	
